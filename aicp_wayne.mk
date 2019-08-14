04dd6b3d7b56cef930853396f27eb8e5fabd5695
nebrassy <nebras30@gmail.com>
2019-08-15 01:08:48 +0300

    move fingerprint to common tree

--- aicp_wayne.mk
+++ aicp_wayne.mk
@@ -35,11 +35,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 
 TARGET_VENDOR_PRODUCT_NAME := wayne
 
-# Build Fingerprint
-PRODUCT_BUILD_PROP_OVERRIDES += \
-	PRIVATE_BUILD_DESC="wayne-user 9 PKQ1.180904.001 V10.3.4.0.PDCCNXM release-keys"
-
-BUILD_FINGERPRINT="xiaomi/wayne/wayne:9/PKQ1.180904.001/V10.3.4.0.PDCCNXM:user/release-keys"
 
 # Device identifier
 PRODUCT_BRAND := Xiaomi
